d16305c987f5d427fd6b802371acab85e8392ed0
zoek <felipe.esbu@gmail.com>
2021-01-17 02:10:26 -0300

    📦

---
+++ src/main/java/com/pruebas/PruebaAplication.java
@@ -0,0 +1,12 @@
+package com.pruebas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaAplication {
+  public static void main(String[] args) {
+    SpringApplication.run(PruebaAplication.class, args);
+  } 
+}
+
---
+++ src/main/java/com/pruebas/rest/PersonaRest.java
@@ -0,0 +1,10 @@
+package com.pruebas.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Rest controller declara personaRest como una clase REST
+@RequestMapping("api/personas") // Request Mapping define la ruta principal de la API
+public class PersonaRest {
+  
+}
